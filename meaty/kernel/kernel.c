f52573a6d87be16e79f26f19b15b3ae9a33d7e31
paulboul1013 <paulboul1013@gmail.com>
2026-04-11 10:26:29 +0800

    Add VGA showing testing

--- meaty/kernel/kernel.c
+++ meaty/kernel/kernel.c
@@ -5,5 +5,10 @@
 
 void kernel_main(void){
     terminal_initialize();
-    terminal_writestring("terminal layer ok");
+    terminal_writestring("line 1\n");
+    terminal_writestring("line 2\n");
+    terminal_writestring("line 3\n");
+    terminal_writestring("abc\ndef\nxyz\n");
+    terminal_writestring("This is a very long line that should continue writing until it wraps to the next VGA row automatically.");
+
 }
