28048fcfdba0d9c130e865884b104231a32879b7
paulboul1013 <paulboul1013@gmail.com>
2026-04-12 11:47:37 +0800

    implement cursur function & replace newline code with function version

--- meaty/kernel/tty.c
+++ meaty/kernel/tty.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "../include/kernel/tty.h"
+#include "../include/kernel/io.h"
 
 static const size_t VGA_WIDTH=80;
 static const size_t VGA_HEIGHT=25;
@@ -9,7 +10,14 @@ static const size_t TAB_WIDTH=4;
 static size_t row;
 static size_t col;
 static uint8_t color;
-static uint16_t* const VGA=(uint16_t*)0xB8000;
+static volatile uint16_t* const VGA=(uint16_t*)0xB8000;
+
+//VGA cursor ports
+#define VGA_CMD_PORT 0x3D4
+#define VGA_DATA_PORT 0x3D5
+
+#define CURSOR_HIGH_BYTE 14
+#define CURSOR_LOW_BYTE 15
 
 static inline uint16_t vga_entry(char c,uint8_t color){
     return (uint16_t)c | (uint16_t)color<<8;
@@ -22,22 +30,32 @@ void terminal_initialize(void){
     for(size_t y=0;y<VGA_HEIGHT;y++){
         terminal_clear_row(y);
     }
+
+    //underline-like cursor
+    terminal_enable_cursor(14,15);
+    terminal_update_cursor();
+}
+
+static void terminal_newline(void){
+    col=0;
+    row++;
+
+    if (row>=VGA_HEIGHT){
+        terminal_scroll();
+        row=VGA_HEIGHT-1;
+    }
 }
 
 void terminal_putchar(char c){
     if (c=='\n'){
-        col=0;
-        row++;
-
-        if (row >= VGA_HEIGHT){
-            terminal_scroll();
-            row=VGA_HEIGHT-1;
-        }
+        terminal_newline();
+        terminal_update_cursor();
         return;
     }
 
     if (c=='\r'){
         col=0;
+        terminal_update_cursor();
         return;
     }
 
@@ -57,14 +75,10 @@ void terminal_putchar(char c){
 
     //if more than VGA_WIDTH(80) , auto move to the next line
     if (col >= VGA_WIDTH){
-        col=0;
-        row++;
-        
-        if (row>=VGA_HEIGHT){
-            terminal_scroll();
-            row=VGA_HEIGHT-1;
-        }
+        terminal_newline();
     }
+
+    terminal_update_cursor();
 }
 
 static void terminal_clear_row(size_t row){
@@ -89,3 +103,22 @@ void terminal_writestring(const char *s){
         terminal_putchar(s[i]);
     }
 }
+
+static void terminal_update_cursor(void){
+    uint16_t pos=(uint16_t)(row*VGA_WIDTH+col);
+
+    outb(VGA_CMD_PORT,CURSOR_HIGH_BYTE);
+    outb(VGA_DATA_PORT,(uint8_t)((pos>>8)&0xFF));
+
+    outb(VGA_CMD_PORT,CURSOR_LOW_BYTE);
+    outb(VGA_DATA_PORT,(uint8_t)(pos&0xFF));
+}
+
+static void terminal_enable_cursor(uint8_t start,uint8_t end){
+    outb(VGA_CMD_PORT,0x0A);
+    outb(VGA_DATA_PORT,(inb(VGA_DATA_PORT)&0xC0)|start);
+
+    outb(VGA_CMD_PORT,0x0B);
+    outb(VGA_DATA_PORT,(inb(VGA_DATA_PORT)&0xE0)|end);
+}
+
