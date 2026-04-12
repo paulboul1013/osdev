12ed222b77032a576666fa35fe72af77f7ef0562
paulboul1013 <paulboul1013@gmail.com>
2026-04-12 11:46:32 +0800

    Add setting cursor function & newline function

--- meaty/include/kernel/tty.h
+++ meaty/include/kernel/tty.h
@@ -6,4 +6,7 @@ void terminal_putchar(char c);
 static void terminal_clear_row(size_t row);
 static void terminal_scroll(void);
 void terminal_writestring(const char *data);
+static void terminal_update_cursor(void);
+static void terminal_enable_cursor(uint8_t start,uint8_t end);
+static void terminal_newline(void);
 #endif
