e723bb31cb57fe1e5b5c82685018af6bcfd08aa8
paulboul1013 <paulboul1013@gmail.com>
2026-04-15 11:53:47 +0800

    Add serial.h define serial function

---
+++ meaty/include/kernel/serial.h
@@ -0,0 +1,12 @@
+#ifndef KERNEL_SERIAL_H
+#define KERNEL_SERIAL_H
+
+#include <stddef.h>
+
+void serial_initialize(void);
+int serial_is_transmit_fifo_empty(void);
+void serial_putchar(char c);
+void serial_write(const char *buf,size_t len);
+void serial_writestring(const char *s);
+
+#endif
