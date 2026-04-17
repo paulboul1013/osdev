b502ed18116994bccc5a958fa91f7a3e4f4b29c6
paulboul1013 <paulboul1013@gmail.com>
2026-04-17 14:31:11 +0800

    Add gdt table definiation

---
+++ meaty/arch/i386/gdt.h
@@ -0,0 +1,23 @@
+#ifndef ARCH_I386_GDT_H
+#define ARCH_I386_GDT_H
+
+#include <stdint.h>
+
+struct gdt_entry{
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
+}__attribute__((packed));
+
+struct gdt_ptr{
+    uint16_t limit;
+    uint32_t base;
+}__attribute__((packed));
+
+void gdt_init(void);
+void gdt_flush(uint32_t gdt_ptr_addr);
+
+#endif
